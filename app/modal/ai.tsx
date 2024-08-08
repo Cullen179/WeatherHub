a61b57ba07a1a2e07cbe853c7b38e943d8b0d202
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-08-08 14:53:27 +0700

    Create ai.tsx

---
+++ app/modal/ai.tsx
@@ -0,0 +1,15 @@
+
+import { Suspense } from "react";
+import { fetchRecommendation } from "./fetch";
+
+export default async function AIRecommendation() {
+    const recommendations = await fetchRecommendation();
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <p>
+                {recommendations.message.content}
+            </p>
+        </Suspense>
+    );
+}
