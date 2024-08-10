3aef1e27e4d809f42638bfdf60050ced7b28a2e8
comet-candy <116716746+s3978128@users.noreply.github.com>
2024-08-11 06:24:23 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,15 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
+  // Skip authentication checks in development mode
+  if (isDevelopment || isPublicRoute(request)) {
+    return;
   }
+
+  // Protect routes if not in development mode
+  auth().protect();
 });
 
 export const config = {
