620b2aa593b0637965103a3faad3fe1510541735
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-08-14 19:48:48 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { SignIn, SignedOut } from '@clerk/nextjs';
-import Image from 'next/image';
+import Forecast from './Forecast';
 
 export default function Home() {
-  return <div className="flex justify-center">Dashboard</div>;
+  return <div>
+    <Forecast />
+  </div>;
 }
