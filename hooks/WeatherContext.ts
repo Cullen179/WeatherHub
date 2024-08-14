e58dff48acfa6a6748f6f94cdbe48dd059ba4cc7
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-08-14 19:48:57 +0700

    Create WeatherContext.ts

---
+++ hooks/WeatherContext.ts
@@ -0,0 +1,8 @@
+import { createContext, useContext } from "react";
+
+export const WeatherContext = createContext<any>(null);
+
+export const useWeather = () => {
+  return useContext(WeatherContext);
+};
+
