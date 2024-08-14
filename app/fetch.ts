721b63215d3848e7c176b38163cfa0c91b02e15f
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-08-14 19:48:42 +0700

    Create fetch.ts

---
+++ app/fetch.ts
@@ -0,0 +1,23 @@
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export async function fetchWeatherData(lat: string, lon: string) {
+  const weatherResponse = await fetch(`${baseUrl}/api/weather?lat=${lat}&lon=${lon}`)
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error('Failed to fetch weather data', error);
+      return null;
+    });
+  return weatherResponse;
+}
+
+export async function fetchForecastData(lat: string, lon: string) {
+  const forecastResponse = await fetch(
+    `${baseUrl}/api/forecast?lat=${lat}&lon=${lon}`
+  )
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error('Failed to fetch forecast data', error);
+      return null;
+    });
+  return forecastResponse;
+}
