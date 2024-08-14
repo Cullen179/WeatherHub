c9f1a80ae65d7d7955eff4a177762885c53fcd3e
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-08-14 19:48:53 +0700

    Create WeatherProvider.tsx

---
+++ app/WeatherProvider.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { WeatherContext } from "@/hooks/WeatherContext";
+
+export default function WeatherProvider({
+  children,
+  weatherData,
+  forecastData,
+}: Readonly<{
+  children: React.ReactNode;
+  weatherData: any;
+  forecastData: any;
+}>) {
+ 
+  return (
+    <WeatherContext.Provider value={{weatherData, forecastData}}>
+      {children}
+    </WeatherContext.Provider>
+  );
+}
