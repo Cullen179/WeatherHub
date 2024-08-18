479bbcb8e04c4d85f67ea2a555718d60bf0fa562
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-08-19 01:41:22 +0700

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,56 +1,9 @@
-'use client';
+import WeatherMap from './WeatherMap';
 
-import { useEffect, useState } from 'react';
-import { fetchWeatherMap } from '../fetch';
-import Image from 'next/image';
-
-const WeatherMap = () => {
-  const [mapUrl, setMapUrl] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const getWeatherMap = async () => {
-      const lat = 10; 
-      const lon = 106; 
-      const zoom = 9; 
-      const mapType = 'clouds_new'; 
-
-      try {
-        const url = await fetchWeatherMap(lat, lon, zoom, mapType);
-        if (url) {
-          setMapUrl(url);
-        } else {
-          setError('Failed to load weather map');
-        }
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getWeatherMap();
-  }, []);
-
-  if (loading) return <p>Loading weather map...</p>;
-  if (error) return <p>{error}</p>;
-
+export default function Map() {
   return (
     <div>
-      <h1>Weather Map</h1>
-      {mapUrl && (
-        <Image
-            src={mapUrl}
-            alt="Weather Map"
-            width={600}  
-            height={400} 
-            layout="responsive" 
-            objectFit="contain" 
-        />
-        )}
+      <WeatherMap />
     </div>
   );
-};
-
-export default WeatherMap;
+}
