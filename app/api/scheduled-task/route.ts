7cb0fa8cc99e0103191f2325dad3540cb414320e
HoangMinh312 <117099173+HoangMinh312@users.noreply.github.com>
2024-08-28 20:05:28 +0700

    in progress 2

--- app/api/scheduled-task/route.ts
+++ app/api/scheduled-task/route.ts
@@ -1,6 +1,12 @@
 import { EmailTemplate } from '@/components/emailTemplates/email-templates';
 import { Resend } from 'resend';
-import { collectionGroup, doc, getDoc, getDocs, query } from 'firebase/firestore';
+import {
+  collectionGroup,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+} from 'firebase/firestore';
 import { db } from '@/firebase';
 import { fetchForecastData } from '@/app/fetch';
 import { Hash } from 'lucide-react';
@@ -23,11 +29,15 @@ export async function POST() {
     for (const alertSettings of querySnapshot.docs) {
       const data = alertSettings.data();
       console.log('User data:', data);
-      
+
       // Check if the city has already been processed
       if (!processedCities.has(data.city)) {
         // Fetch forecast data for the city
-        const forecastData = await fetchForecastData(undefined, undefined, data.city);
+        const forecastData = await fetchForecastData(
+          undefined,
+          undefined,
+          data.city
+        );
         processedCities.set(data.city, forecastData);
 
         // Check if the user needs to be notified
@@ -62,33 +72,89 @@ export async function POST() {
     }
   } catch (error) {
     console.error('Failed to fetch user data:', error);
-    return NextResponse.json({ error: 'Failed to fetch user data' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to fetch user data' },
+      { status: 500 }
+    );
   }
 
-  
-
   // Send notifications to users
+
+  // EMAIL
+  const entries = Array.from(dataToNotify.entries());
   try {
-    const entries = Array.from(dataToNotify.entries());
 
     for (const [notification, emails] of entries) {
       const { data, error } = await resend.emails.send({
         from: 'WeatherHub <onboarding@resend.dev>',
-        to: emails,
+        to: 's3977773@rmit.edu.vn',
         subject: 'WeatherHub Alert!',
         react: EmailTemplate(notification),
       });
 
       if (error) {
         console.error('Failed to send email:', error);
-        return NextResponse.json({ error: 'Failed to send email' }, { status: 500 });
+        return NextResponse.json(
+          { error: 'Failed to send email' },
+          { status: 500 }
+        );
       }
     }
-    return NextResponse.json({ message: 'Notifications sent successfully' }, { status: 200 });
   } catch (error) {
     console.error('Failed to send notifications:', error);
-    return NextResponse.json({ error: 'Failed to send notifications' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to send notifications' },
+      { status: 500 }
+    );
   }
+
+  // SMS
+  const INFOBIP_API_URL = 'https://43dgrn.api.infobip.com/sms/2/text/advanced';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Authorization', `App ${process.env.INFOBIP_API_KEY}`);
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Accept', 'application/json');
+
+  let textData = '';
+
+  for (const [notification, emails] of entries) {
+    // Introduction
+    textData += `WeatherHub weather alert!\n\n`;
+
+    // Capitalize the first letter of the notification type
+    notification.type = notification.type.charAt(0).toUpperCase() + notification.type.slice(1);
+
+    textData += `${notification.type} value is ${notification.value} at ${notification.date}\n`;
+  }
+
+  const raw = JSON.stringify({
+    messages: [
+      {
+        destinations: [{ to: '84913237666' }],
+        from: '447491163443',
+        text: textData,
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+  };
+
+  const response = await fetch(INFOBIP_API_URL, requestOptions);
+  const responseData = await response.json();
+  if (response.status !== 200) {
+    console.error('Failed to send SMS:', responseData);
+    return NextResponse.json({ error: 'Failed to send SMS' }, { status: 500 });
+  }
+
+  return NextResponse.json(
+    { message: 'Notifications sent successfully' },
+    { status: 200 }
+  );
 }
 
 // Function to check if the user needs to be notified and when to notify what data
@@ -112,16 +178,24 @@ function checkNotification(data: any, forecastData: any) {
     rainChance *= 100; // Convert to percentage
     const rainVolume = forecast.rain?.['3h'] ? forecast.rain['3h'] : 0; // Rain volume in the last 3 hours if available
 
-
     // Check if the forecast meets the criteria
-    if (temperature < data.temperatures.range[0] || temperature > data.temperatures.range[1]) {
+    if (
+      temperature < data.temperatures.range[0] ||
+      temperature > data.temperatures.range[1]
+    ) {
       notifications.push({ type: 'temperature', value: temperature, date });
     }
-    if (humidity < data.humidity.range[0] || humidity > data.humidity.range[1]) {
+    if (
+      humidity < data.humidity.range[0] ||
+      humidity > data.humidity.range[1]
+    ) {
       notifications.push({ type: 'humidity', value: humidity, date });
     }
 
-    if (seaPressure < data.seaPressure.range[0] || seaPressure > data.seaPressure.range[1]) {
+    if (
+      seaPressure < data.seaPressure.range[0] ||
+      seaPressure > data.seaPressure.range[1]
+    ) {
       notifications.push({ type: 'seaPressure', value: seaPressure, date });
     }
 
@@ -138,7 +212,11 @@ function checkNotification(data: any, forecastData: any) {
     }
 
     if (rainVolume > data.rainVolume.range[1]) {
-      notifications.push({ type: 'rainVolume', value: forecast.rain['3h'], date });
+      notifications.push({
+        type: 'rainVolume',
+        value: forecast.rain['3h'],
+        date,
+      });
     }
   }
 
