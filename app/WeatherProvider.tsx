c6e20d029d08ac3c69b8ad0d9c055a796423e97a
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-09-01 19:03:59 +0700

    Update WeatherProvider.tsx

--- app/WeatherProvider.tsx
+++ app/WeatherProvider.tsx
@@ -3,6 +3,11 @@ import { WeatherContext } from '@/hooks/WeatherContext';
 import { fetchForecastData, fetchWeatherData } from './fetch';
 import { useEffect, useState } from 'react';
 
+interface geoLocation {
+  latitude: number;
+  longitude: number;
+}
+
 export default function WeatherProvider({
   children,
 }: Readonly<{
@@ -10,32 +15,43 @@ export default function WeatherProvider({
 }>) {
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
+  const [geoLocation, setGeoLocation] = useState<geoLocation | null>(null);
+
+  // Function to fetch data
+  const fetchData = async (latitude: number, longitude: number) => {
+    try {
+      console.log('Fetching weather data for:', { latitude, longitude });
+      const weatherData = await fetchWeatherData(latitude, longitude);
+      const forecastData = await fetchForecastData(latitude, longitude);
+      setWeatherData(weatherData);
+      setForecastData(forecastData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && 'geolocation' in navigator) {
+    if (geoLocation) {
+      const { latitude, longitude } = geoLocation;
+      fetchData(latitude, longitude);
+    } else if (typeof window !== 'undefined' && 'geolocation' in navigator) {
+      console.log('GeoLocation is null, requesting user location...');
       navigator.geolocation.getCurrentPosition(
-        async (position) => {
+        (position) => {
           const { latitude, longitude } = position.coords;
-          console.log('Latitude:', latitude);
-          console.log('Longitude:', longitude);
+          console.log('Setting GeoLocation:', { latitude, longitude });
 
-          const weatherData = await fetchWeatherData(latitude, longitude);
-          const forecastData = await fetchForecastData(latitude, longitude);
-
-          setWeatherData(weatherData);
-          setForecastData(forecastData);
+          setGeoLocation({ latitude, longitude });
         },
         (error) => {
           console.error('Error getting location:', error);
         }
       );
-    } else {
-      console.error('Geolocation is not supported');
     }
-  }, []);
+  }, [geoLocation]);
 
   return (
-    <WeatherContext.Provider value={{ weatherData, forecastData }}>
+    <WeatherContext.Provider value={{ weatherData, forecastData, geoLocation, setGeoLocation }}>
       {children}
     </WeatherContext.Provider>
   );
