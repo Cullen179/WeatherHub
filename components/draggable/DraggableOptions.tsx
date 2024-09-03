2a4be44503ee17f0b010e23e02562b7e7af71ec6
comet-candy <116716746+s3978128@users.noreply.github.com>
2024-09-03 19:04:06 +0700

    bug fixed

--- components/draggable/DraggableOptions.tsx
+++ components/draggable/DraggableOptions.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { useDrag } from 'react-dnd';
 import { Button } from '@/components/ui/button';
 
@@ -13,6 +13,7 @@ const DraggableOptions: FC<DraggableOptionsProps> = ({
   option,
   displayName,
 }) => {
+  const ref = useRef<HTMLButtonElement>(null);
   const [{ isDragging }, drag] = useDrag(() => ({
     type: 'widget',
     item: { option },
@@ -21,10 +22,12 @@ const DraggableOptions: FC<DraggableOptionsProps> = ({
     }),
   }));
 
+  drag(ref);
+
   return (
     <Button
       variant="outline"
-      ref={drag}
+      ref={ref}
       className={`mb-2 w-full justify-start pl-2 ${
         isDragging ? 'opacity-50 cursor-grabbing' : 'opacity-100 cursor-grab'
       }`}
