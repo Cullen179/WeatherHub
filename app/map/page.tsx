0d0e979047b4dd0e32dc494dba5503a21b041ec4
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-09-06 22:10:58 +0700

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,8 +2,10 @@ import WeatherMap from './WeatherMap';
 
 export default function Map() {
   return (
-    <div>
-      <WeatherMap />
+    <div className="relative">
+      <div className="flex flex-col absolute -top-4 -left-10 w-screen h-[calc(100vh-53px)]">
+        <WeatherMap />
+      </div>
     </div>
   );
 }
