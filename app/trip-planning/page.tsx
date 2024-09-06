fefff62aa8456fc08bb3311d28e9e16cbefc5f48
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-09-06 22:11:02 +0700

    Update page.tsx

--- app/trip-planning/page.tsx
+++ app/trip-planning/page.tsx
@@ -1,9 +1,17 @@
-export default function TripPlanning() {
+import WeatherMap from '../map/WeatherMap';
+import Sheet from './Sheet';
 
-    return (
-        <div>
-            <h1>Trip Planning</h1>
-            <p>Plan your trip here with us!</p>
+export default function TripPlanning() {
+  return (
+    <div className="flex grow h-full">
+      <div className="w-[30%] flex border-border border-r-2">
+        <Sheet />
+      </div>
+      <div className="w-[70%] relative">
+        <div className="flex flex-col absolute w-full h-full">
+          <WeatherMap showSearch={false} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
