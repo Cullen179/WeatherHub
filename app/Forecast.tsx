320b6bc732be46e5ca2f8069854293aa7b426110
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2024-09-06 22:09:41 +0700

    Update Forecast.tsx

--- app/Forecast.tsx
+++ app/Forecast.tsx
@@ -39,9 +39,11 @@ export default function Forecast() {
         // Calculate actual min and max temperatures for the day
         const minTemp = Math.min(...dayData.map((item) => item.main.temp_min));
         const maxTemp = Math.max(...dayData.map((item) => item.main.temp_max));
+        const avgTemp = (minTemp + maxTemp) / 2;
 
         return {
           date, // The date is already in ISO format, no need to adjust
+          avgTemp: avgTemp.toFixed(0),
           minTemp: minTemp.toFixed(0),
           maxTemp: maxTemp.toFixed(0),
           main: dayData[0].weather[0].main,
@@ -59,89 +61,89 @@ export default function Forecast() {
       ) : (
         <>
           <div className='flex justify-between'>
-            <h1 className="text-3xl font-bold mb-3">{weatherData.name}</h1>
+            <h2 className="text-2xl font-bold mb-3">Week Forecast</h2>
             <div className="flex items-center p-4">
               <SearchCity onCoordinatesFound={handleCoordinatesFound} />
             </div>
           </div>
-          <h2 className="text-2xl font-bold mb-3">Week Forecast</h2>
-          <div className="flex gap-2">
-            <Card className="flex items-center basis-2/6 drop-shadow-md rounded-md">
-              <CardHeader className="basis-3/5">
-                <CardTitle>
-                  Today:{' '}
-                  {
-                    new Date(weatherData.dt * 1000)
+          <div className="flex w-[50%] gap-2">
+            <Card className="basis-3/6 drop-shadow-md rounded-md">
+              <CardHeader>
+                <div className="flex justify-between items-center mb-2">
+                  <CardTitle className="text-sm">{weatherData.name}</CardTitle>
+                  <CardDescription className='basis-2/3 flex justify-end'>
+                    {new Date(weatherData.dt * 1000).toLocaleTimeString('en-EN', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      second: '2-digit',
+                      hour12: true,
+                    })}
+                  </CardDescription>
+                </div>
+                  <div className='text-sm basis-1/3'>
+                    {"Today: "}
+                    {
+                      new Date(weatherData.dt * 1000)
                       .toLocaleDateString('en-GB', {
-                        weekday: 'short',
-                        day: '2-digit',
-                        month: 'short',
+                        weekday: 'long',
                       })
                       .split('T')[0]
-                  }
-                </CardTitle>
-                <CardDescription>
-                  {new Date(weatherData.dt * 1000).toLocaleTimeString('vi-VN', {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: true,
-                  })}
-                </CardDescription>
-                <p className="capitalize text-sm">
-                  {weatherData.weather[0].description}
-                </p>
+                    }
+                  </div>
               </CardHeader>
+                <h2 className="flex justify-center text-6xl my-5 font-bold">
+                  {weatherData.main.temp.toFixed(0)}°
+                </h2>  
               <CardContent className="basis-2/5">
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col">
                   <Image
                     src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-                    width={100}
-                    height={100}
+                    width={40}
+                    height={40}
                     alt={weatherData.description}
                   />
+                  <p className="capitalize text-sm">
+                    {weatherData.weather[0].description}
+                  </p>
                   <div className="flex">
-                    <p className="text-sm">
-                      {weatherData.main.temp_min}°C -{' '}
-                      {weatherData.main.temp_max}°C
+                    <p className="text-sm text-muted-foreground">
+                      {"L: "}
+                      {weatherData.main.temp_min.toFixed(0)}°
+                      {" H: "}
+                      {weatherData.main.temp_max.toFixed(0)}°
                     </p>
                   </div>
                 </div>
               </CardContent>
             </Card>
-            {calculateDailyAverages(forecastData.list).map(
-              (day: any, index: number) => (
-                <Card
-                  className="flex flex-col items-center basis-1/6 drop-shadow-md rounded-md py-2 px-4"
-                  key={index}
-                >
-                  <h2 className="font-bold">
-                    {new Date(day.date).toLocaleDateString('en-GB', {
-                      weekday: 'short',
-                      day: '2-digit',
-                      month: 'short',
-                    })}
-                  </h2>
-                  <Image
-                    src={`http://openweathermap.org/img/wn/${day.icon}@2x.png`}
-                    width={100}
-                    height={100}
-                    alt={day.main}
-                  />
-                  <p className="capitalize text-sm text-muted-foreground">
-                    {day.main}
-                  </p>
-                  <div className="flex items-center gap-2 w-full">
-                    <p className="text-sm">{day.minTemp}°C </p>
-                    <ConditionLevel
-                      type="Temperature"
-                      startNum={parseInt(day.minTemp)}
-                      endNum={parseInt(day.maxTemp)}
+            <Card className='flex'>
+              {calculateDailyAverages(forecastData.list).map(
+                (day: any, index: number) => (
+                  <Card
+                    className="flex flex-col items-center justify-between basis-1/4 border-0 rounded-md py-6 px-4"
+                    key={index}
+                  >
+                    <h2 className="font-bold">
+                      {index === 0
+                        ? "Tomorrow"
+                        : new Date(day.date).toLocaleDateString('en-GB', {
+                            weekday: 'short',
+                        })
+                      }
+                    </h2>
+                    <Image
+                      src={`http://openweathermap.org/img/wn/${day.icon}@2x.png`}
+                      width={60}
+                      height={60}
+                      alt={day.main}
                     />
-                    <p className="text-sm">{day.maxTemp}°C </p>
-                  </div>
-                </Card>
-              )
-            )}
+                    <div className="flex justify-center w-full">
+                      <p className="text-sm">{day.avgTemp}° </p>
+                    </div>
+                  </Card>
+                )
+              )}
+            </Card>
           </div>
         </>
       )}
