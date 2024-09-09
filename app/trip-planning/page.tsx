f9d69dc6f636219c18362e8cccdb687824763e1f
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-09-09 22:03:24 +0700

    Update page.tsx

--- app/trip-planning/page.tsx
+++ app/trip-planning/page.tsx
@@ -1,17 +1,19 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
 import WeatherMap from '../map/WeatherMap';
 import Sheet from './Sheet';
 
 export default function TripPlanning() {
   return (
-    <div className="flex grow">
-      <div className="w-[30%] flex border-border border-r-2 pr-3">
-        <Sheet />
+      <div className="flex grow">
+          <ScrollArea className="w-[30%] flex border-border border-r-2 pr-3 h-[calc(100vh-76.667px)]">
+              <Sheet />
+          </ScrollArea>
+
+          <div className="w-[70%] relative">
+              <div className="flex flex-col absolute w-full h-full">
+                  {/* <WeatherMap showSearch={false} /> */}
+              </div>
+          </div>
       </div>
-      <div className="w-[70%] relative">
-        <div className="flex flex-col absolute w-full h-full">
-          {/* <WeatherMap showSearch={false} /> */}
-        </div>
-      </div>
-    </div>
   );
 }
