e42e753a59ed879ab3ce7b09548945adf215e3d0
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-09-15 21:59:17 +0700

    Update page.tsx

--- app/alert/page.tsx
+++ app/alert/page.tsx
@@ -4,8 +4,7 @@ import { Typography } from '@/components/Typography';
 export default function AlertSettings() {
   return (
     <div className="space-y-4">
-      <Typography variant="h1">Alert</Typography>
-      <Typography variant="h2">Parameters Setting</Typography>
+      <Typography variant="h2">Weather Trigger Settings</Typography>
       <AlertForm />
     </div>
   );
